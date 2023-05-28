e0927d50f530e93bef779d8bb33755f551bc4450
Vencislav Peev <vencislavpeev18.8@gmail.com>
2023-05-28 22:56:00 +0300

    Finished default Modal and now working on RegisterModal

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -96,7 +96,18 @@ const Modal: React.FC<ModalProps> = ({
                      {/* FOOTER */}
                      <div className="flex flex-col gap-2 p-6">
                         <div className="flex flex-row items-center gap-4 w-full">
-                           <Button label="Hi"/>
+                           {secondaryAction && secondaryActionLabel && (
+                              <Button 
+                              disabled={disabled}
+                              label={secondaryActionLabel}
+                              onClick={handleSecondaryAction}
+                              />
+                           )}
+                           <Button 
+                              disabled={disabled}
+                              label={actionLabel}
+                              onClick={handleSubmit}
+                           />
                         </div>
                      </div>
                   </div>
